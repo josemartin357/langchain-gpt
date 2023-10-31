25a21b362c7017d023377fe9aaafc0f763b3b3cf
Martin Bedoya <83382332+josemartin357@users.noreply.github.com>
2023-10-30 23:18:51 -0700

    Using SimpleSequentialChain

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -18,13 +18,19 @@ title_template = PromptTemplate(
     template = 'write me a script about {topic}'
 )
 
+script_template = PromptTemplate(
+    input_variables = ['title'], 
+    template = 'write me a script based on this title {title}'
+)
+
 # LLMs
 llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True) # setting verbose to true so output is more readable
-
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True) 
+sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True) # chain run title_chain first to generate video title for script_chain 
 
 # Showing response on screen if there is a response
 if prompt:
-    response = title_chain.run(topic=prompt)
+    response = sequential_chain.run(prompt) # passing sequential chain as response
     st.write(response)
 
